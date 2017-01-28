0358a7cab7563a4901f7eff9bfe076883d563ff0
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-28 13:02:44 +0300

    Initial Commit

---
+++ Timers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timers
+//
+//  Created by Atıl Samancıoğlu on 28/01/2017.
+//  Copyright © 2017 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
