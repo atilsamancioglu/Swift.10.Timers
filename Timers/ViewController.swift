6b6cdcddcc4c18212b3108a693fa33f2151a1f6c
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:16:32 +0300

    final version

--- Timers/ViewController.swift
+++ Timers/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        counter = 10
+        timerLabel.text = String(counter)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunction), userInfo: nil, repeats: true)
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func timerFunction() {
+        
+        // our function for timer
+        timerLabel.text = String(counter)
+        counter = counter - 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timerLabel.text = "Time is done!"
+        }
+        
     }
-
+    
 
 }
 
